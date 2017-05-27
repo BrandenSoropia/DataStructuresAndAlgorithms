36b62c12ecf445068fc74ab241ea2850c8e00028
Branden Soropia <b.soropia@gmail.com>
2017-05-27 10:54:01 -0400

    Tested init with 3 nodes

--- graphs/test_binary_search_tree.py
+++ graphs/test_binary_search_tree.py
@@ -52,14 +52,14 @@ class TestBinarySearchTree(unittest.TestCase):
         new_nodes = create_nodes([2, 3])
         bst = BinarySearchTree(new_nodes)
         root = bst.get_root()
-        root_right_node = root.get_right()
+        root_right_child = root.get_right()
 
         # Depth 0
         self.assertIsNone(root.get_left())
         # Depth 1
-        self.assertEqual(root_right_node.get_value(), 3)
-        self.assertTrue(is_leaf_node(root_right_node))
-        self.assertEqual(root_right_node.get_parent(), root)
+        self.assertEqual(root_right_child.get_value(), 3)
+        self.assertTrue(is_leaf_node(root_right_child))
+        self.assertEqual(root_right_child.get_parent(), root)
 
     def test_initiate_with_three_nodes_where_child_is_less_than_parent(self):
         new_nodes = create_nodes([3, 2, 1])
@@ -71,11 +71,117 @@ class TestBinarySearchTree(unittest.TestCase):
         self.assertIsNone(root.get_right())
         # Depth 1
         self.assertEqual(root_left_child.get_value(), 2)
-        self.assertIsNone(root_left_child.get_parent().get_right())
+        self.assertIsNone(root_left_child.get_right())
         # Depth 2
         self.assertEqual(root_left_child.get_left().get_value(), 1)
         self.assertTrue(is_leaf_node(root_left_child.get_left()))
         self.assertEqual(root_left_child.get_left().get_parent(), root_left_child)
+
+    def test_initiate_with_three_nodes_with_same_value(self):
+        new_nodes = create_nodes([1, 1, 1])
+        bst = BinarySearchTree(new_nodes)
+        root = bst.get_root()
+        root_left_child = root.get_left()
+
+        # Depth 0
+        self.assertIsNone(root.get_right())
+        # Depth 1
+        self.assertEqual(root_left_child.get_value(), 1)
+        self.assertIsNone(root_left_child.get_right())
+        # Depth 2
+        self.assertEqual(root_left_child.get_left().get_value(), 1)
+        self.assertTrue(is_leaf_node(root_left_child.get_left()))
+        self.assertEqual(root_left_child.get_left().get_parent(), root_left_child)
+
+    def test_init_with_three_nodes_with_one_less_than_root_and_other_greater_than_child_but_less_than_root(self):
+        new_nodes = create_nodes([3, 1, 2])
+        bst = BinarySearchTree(new_nodes)
+        root = bst.get_root()
+        root_left_child = root.get_left()
+
+        # Depth 0
+        self.assertIsNone(root.get_right())
+        # Depth 1
+        self.assertEqual(root_left_child.get_value(), 1)
+        self.assertIsNone(root_left_child.get_left())
+        # Depth 2
+        self.assertEqual(root_left_child.get_right().get_value(), 2)
+        self.assertTrue(is_leaf_node(root_left_child.get_right()))
+        self.assertEqual(root_left_child.get_right().get_parent(), root_left_child)
+
+    def test_init_with_three_nodes_first_insert_less_than_root_and_next_insert_greater_than_root(self):
+        new_nodes = create_nodes([2, 1, 3])
+        bst = BinarySearchTree(new_nodes)
+        root = bst.get_root()
+        root_left_child = root.get_left()
+        root_right_child = root.get_right()
+
+        # Depth 1
+        self.assertEqual(root_left_child.get_value(), 1)
+        self.assertTrue(is_leaf_node(root_left_child))
+        self.assertEqual(root_left_child.get_parent(), root)
+
+        self.assertEqual(root_right_child.get_value(), 3)
+        self.assertTrue(is_leaf_node(root_right_child))
+        self.assertEqual(root_right_child.get_parent(), root)
+
+    def test_init_with_three_nodes_first_insert_greater_than_root_and_next_insert_less_than_root(self):
+        new_nodes = create_nodes([2, 3, 1])
+        bst = BinarySearchTree(new_nodes)
+        root = bst.get_root()
+        root_left_child = root.get_left()
+        root_right_child = root.get_right()
+
+        # Depth 1
+        self.assertEqual(root_left_child.get_value(), 1)
+        self.assertTrue(is_leaf_node(root_left_child))
+        self.assertEqual(root_left_child.get_parent(), root)
+
+        self.assertEqual(root_right_child.get_value(), 3)
+        self.assertTrue(is_leaf_node(root_right_child))
+        self.assertEqual(root_right_child.get_parent(), root)
+
+    def test_init_with_three_nodes_first_insert_greater_than_root_next_greater_than_root_but_less_than_child(self):
+        new_nodes = create_nodes([1, 3, 2])
+        bst = BinarySearchTree(new_nodes)
+        root = bst.get_root()
+        root_right_child = root.get_right()
+
+        # Depth 1
+        self.assertEqual(root_right_child.get_value(), 3)
+        self.assertIsNone(root_right_child.get_right())
+        # Depth 2
+        self.assertEqual(root_right_child.get_left().get_value(), 2)
+        self.assertTrue(is_leaf_node(root_right_child.get_left()))
+        self.assertEqual(root_right_child.get_left().get_parent(), root_right_child)
+
+    def test_init_with_three_nodes_with_two_nodes_same_value_and_greater_than_root(self):
+        new_nodes = create_nodes([1, 2, 2])
+        bst = BinarySearchTree(new_nodes)
+        root = bst.get_root()
+        root_right_child = root.get_right()
+
+        # Depth 1
+        self.assertEqual(root_right_child.get_value(), 2)
+        self.assertIsNone(root_right_child.get_right())
+        # Depth 2
+        self.assertEqual(root_right_child.get_left().get_value(), 2)
+        self.assertTrue(is_leaf_node(root_right_child.get_left()))
+        self.assertEqual(root_right_child.get_left().get_parent(), root_right_child)
+
+    def test_init_with_three_nodes_where_child_is_greater_than_parent(self):
+        new_nodes = create_nodes([1, 2, 3])
+        bst = BinarySearchTree(new_nodes)
+        root = bst.get_root()
+        root_right_child = root.get_right()
+
+        # Depth 1
+        self.assertEqual(root_right_child.get_value(), 2)
+        self.assertIsNone(root_right_child.get_left())
+        # Depth 2
+        self.assertEqual(root_right_child.get_right().get_value(), 3)
+        self.assertTrue(is_leaf_node(root_right_child.get_right()))
+        self.assertEqual(root_right_child.get_right().get_parent(), root_right_child)
 
 if __name__ == '__main__':
     unittest.main()
